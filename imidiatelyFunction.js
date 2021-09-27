bb10f8dcc7449fd170df4a49d59077db62c59e60
Gaurav Kumar <Gaurav.chaudhary440@gmail.com>
2021-09-27 15:40:53 +0530

    Create imidiatelyFunction.js

---
+++ imidiatelyFunction.js
@@ -0,0 +1,9 @@
+"use strict ";
+
+//When we want function which will be only executed once So we use imidiatedly called function
+//The function doesnot have any name.
+//The function is wrap with () and to call it immidiately we use () at the end
+
+(function () {
+  console.log("I am an imidiately execute function");
+})();
