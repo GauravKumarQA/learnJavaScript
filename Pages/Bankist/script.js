10868f6598648443e355e80ddb1dcb7a3a288346
Gaurav Kumar <Gaurav.chaudhary440@gmail.com>
2021-10-02 01:06:41 +0530

    Added login validation using javascript not secured

--- Pages/Bankist/script.js
+++ Pages/Bankist/script.js
@@ -4,26 +4,33 @@ const account1 = {
   name: "Gaurav",
   moments: [100, 30, 20, -50, -190, 100, 5000],
   interestRates: 1.7,
+  pass: "123",
 };
 
 const account2 = {
   name: "Chandrika",
   moments: [-100, -30, -20, -50, -190, 100, 5000],
   interestRates: 0.7,
+  pass: "123",
 };
 
 const account3 = {
   name: "Gauravika",
   moments: [200, 45, -20, -50, -190, 100, 1000],
   interestRates: 0.7,
+  pass: "123",
 };
 
+const listOfAccounts = [account1, account2, account3];
+
+const userName = document.querySelector("input[placeholder='Username']");
+const papssword = document.querySelector("input[placeholder='password']");
 const signIn = document.querySelector("#signinform > button");
 const name = document.querySelector("body > div:nth-child(1) > h3");
-name.textComponet;
 const currentDate = document.querySelector("#currentBlnc > p");
 const balanace = document.querySelector("#currentBlnc > h3.currentMoney");
 const table = document.querySelector("table");
+const grettingHeading = document.querySelector(".greetings");
 
 const transferButton = document.querySelector(
   "#trfr > h3:nth-child(2) > button"
@@ -46,8 +53,43 @@ const pinClose = document.querySelector(
   "#cls > h3:nth-child(2) > input[type=password]:nth-child(2)"
 );
 
-const appData = document.querySelector("#centerContent");
+const app = document.querySelector("#centerContent");
 
-const getUserNamePassword = function () {
-  signIn.addEventListener("click", function () {});
+const appEntries = document.querySelector(".entires");
+
+const displayMoments = function (movements) {
+  appEntries.innerHTML = "";
+  movements.forEach(function (element, i) {
+    const type = element > 0 ? "table-success" : "table-danger";
+    const html = `<tr class="${type}">
+    <th scope="row">${i + 1}</th>
+    <td>Debit</td>
+   <td>01/2/2992</td>
+    <td>${element} $</td>
+  </tr>`;
+    appEntries.insertAdjacentHTML("beforeend", html);
+  });
 };
+
+const updateGreeting = function (name) {
+  grettingHeading.textContent = `Greeting ${name}`;
+};
+
+const updateBalance = function (moments) {
+  balanace.textContent = moments.reduce((a, b) => a + b, 0) + "$";
+};
+
+const validateUser = function () {
+  const currentUserName = userName.value;
+  const currentPassword = papssword.value;
+  listOfAccounts.forEach(function (element) {
+    if (element.name === currentUserName && element.pass === currentPassword) {
+      displayMoments(element.moments);
+      updateBalance(element.moments);
+      updateGreeting(element.name);
+      app.setAttribute("style", "opacity:1");
+    }
+  });
+};
+
+signIn.addEventListener("click", validateUser);
